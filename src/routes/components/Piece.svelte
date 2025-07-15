d9e01360b86930b65b89a0f172fe5d4f5b2c5595
Sviatoslav <shot.ngh@gmail.com>
2025-07-15 11:47:49 +0300

    feat: add piece component

---
+++ src/routes/components/Piece.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	import { cn } from '../../utils';
+	import type { PieceColor, PieceId } from '../types';
+
+	interface Props {
+		className?: string;
+		id: PieceId;
+		color: PieceColor;
+	}
+
+	let { className: classNameInput, id, color }: Props = $props();
+	let pieceSrc = $derived.by(() => {
+		const colorCode = color === 'white' ? 'W' : 'B';
+		return `${base}/pieces/${colorCode}${id}.svg`;
+	});
+</script>
+
+<div class={cn('flex h-20 w-20 items-center justify-center', classNameInput)}>
+	<img src={pieceSrc} alt={id} class="h-16 w-16" />
+</div>
---
+++ src/routes/types.ts
@@ -0,0 +1,9 @@
+export type PieceColor = 'white' | 'black';
+
+export type PieceId =
+	| 'p' // Pawn
+	| 'K' // King
+	| 'Q' // Queen
+	| 'R' // Rook
+	| 'B' // Bishop
+	| 'N'; // Knight
