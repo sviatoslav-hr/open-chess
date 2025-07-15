91740f3eb61563601e946e20a2975fe9ceed48fe
Sviatoslav <shot.ngh@gmail.com>
2025-07-15 11:29:27 +0300

    feat: improve board rotation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import Board from './components/Board.svelte';
+	import Button from './components/Button.svelte';
+
+	let boardRotated = $state(false);
 </script>
 
 <main class="flex grow flex-col items-center justify-center">
-	<Board />
+	<Board {boardRotated} />
+	<div class="fixed top-4 right-4 flex justify-center gap-2">
+		<Button onClick={() => (boardRotated = !boardRotated)}>Rotate</Button>
+	</div>
 </main>
--- src/routes/components/Board.svelte
+++ src/routes/components/Board.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import { cn } from '../../utils';
-	import Button from './Button.svelte';
+
+	interface Props {
+		className?: string;
+		boardRotated?: boolean;
+	}
+
+	let { className: classNameInput, boardRotated }: Props = $props();
 
 	const rows = [1, 2, 3, 4, 5, 6, 7, 8] as const;
 	const cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'] as const;
@@ -11,44 +17,38 @@
 	function isOdd(num: number): boolean {
 		return num % 2 !== 0;
 	}
-	let boardRotated = $state(false);
 </script>
 
-<div class="flex w-full justify-center">
-	<div class={cn('flex h-full justify-around', boardRotated ? 'flex-col' : 'flex-col-reverse')}>
+<!-- TODO: Make this scale up the screen size. -->
+
+<div class={cn('flex w-full justify-center', classNameInput)}>
+	<div class={cn('flex h-full w-5 justify-around', boardRotated ? 'flex-col' : 'flex-col-reverse')}>
 		{#each rows as row}
-			<div class="flex h-20 items-center pr-2 text-center">
-				{row}
-			</div>
+			<div class="flex h-20 items-center pr-2 text-center">{row}</div>
 		{/each}
 	</div>
-	<div class={cn('flex', boardRotated ? 'flex-col' : 'flex-col-reverse')}>
+
+	<div class={cn('flex pr-5', boardRotated ? 'flex-col' : 'flex-col-reverse')}>
 		{#each rows as row}
-			<div class="flex">
-				{#each boardRotated ? cols.toReversed() : cols as col, colIndex}
+			<div class={cn('flex', { 'flex-row-reverse': boardRotated })}>
+				{#each cols as col, colIndex}
 					<div
-						class={cn(
-							'flex h-20 w-20 items-center justify-center border-t border-l border-teal-500 last:border-r last:border-l',
-							{
-								'bg-teal-500':
-									isEven(row) !== boardRotated ? isOdd(colIndex + 1) : isEven(colIndex + 1),
-								'border-b': boardRotated ? row === 8 : row === 1
-							}
-						)}
+						class={cn('flex h-20 w-20 items-center justify-center border-teal-500', {
+							'bg-teal-500': isEven(row) ? isOdd(colIndex + 1) : isEven(colIndex + 1),
+							'border-t': row === (boardRotated ? 1 : 8),
+							'border-b': row === (boardRotated ? 8 : 1),
+							'border-r': col === (boardRotated ? 'a' : 'h'),
+							'border-l': col === (boardRotated ? 'h' : 'a')
+						})}
 					></div>
 				{/each}
 			</div>
 		{/each}
 	</div>
 </div>
-<div class="flex w-full justify-center">
-	<div class="w-8"></div>
+
+<div class={cn('flex w-full justify-center', { 'flex-row-reverse': boardRotated })}>
 	{#each cols as col}
-		<div class="w-20 pt-2 text-center">
-			{col}
-		</div>
+		<div class="w-20 pt-2 text-center">{col}</div>
 	{/each}
-</div>
-<div class="fixed top-4 right-4 flex justify-center gap-2">
-	<Button onClick={() => (boardRotated = !boardRotated)}>Rotate</Button>
 </div>
